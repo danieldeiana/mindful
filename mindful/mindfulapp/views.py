60ac00756fa952234a22b788acfdbc4be7a9b510
Daniel Deiana <danieldeiana84@gmail.com>
2019-06-18 11:50:14 +0100

    added template amongst other minor things

--- mindful/mindfulapp/views.py
+++ mindful/mindfulapp/views.py
@@ -1,5 +1,4 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-	return HttpResponse("Hi!")
+	return render(request, 'mindfulapp/index.html')
